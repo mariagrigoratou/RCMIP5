48465b8d9cdc73872b51b6a78c7ee7ed983b05e7
Robert Wilson <robertwilson190@gmail.com>
2017-02-10 11:57:25 +0000

    Update loadEnsemble.R

--- R/loadEnsemble.R
+++ R/loadEnsemble.R
@@ -194,7 +194,9 @@ loadEnsemble <- function(variable, model, experiment, ensemble, domain,
             }
           # More hacking....
           # Some of the year lengths for models are wrong
-            if(stringr::str_detect(model, "EC-EARTH") | stringr::str_detect(model,"CNRM-CM5")| stringr::str_detect(model,"MPI-ESM")  ){
+            if(stringr::str_detect(model, "EC-EARTH") | stringr::str_detect(model,"CNRM-CM5")| stringr::str_detect(model,"MPI-ESM") 
+              | stringr::str_detect(model,"GFDL-CM2")
+              ){
             calendarDayLength <- 365.25
             }
 
